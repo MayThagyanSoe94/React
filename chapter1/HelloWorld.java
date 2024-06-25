2553fd4558acfff8f22f33da62a7341299d35a74
May Thagyan <may.thagyan@privasia.com>
2024-06-25 19:15:59 +0800

    chapter 1

---
+++ chapter1/HelloWorld.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class HelloWorld{
+
+    public static void main(String[] args) {
+        int result = 5/2;
+        System.out.println("Hello World");
+        System.out.println("Result >>> " + result);
+    }
+}
